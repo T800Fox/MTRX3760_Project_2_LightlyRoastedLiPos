f059f482df8000a2cdc91eff228ffe9c4202e939
T800Fox <zorrito5@bigpond.com>
2025-10-23 13:38:03 +1100

    working amcl configured

--- launch/solo_amcl_launch.py
+++ launch/solo_amcl_launch.py
@@ -1,3 +1,6 @@
+# ros2 run nav2_map_server map_saver_cli -f <map_name>
+# ros2 run nav2_map_server map_server --ros-args -p yaml_filename:=my_map.yaml
+
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
